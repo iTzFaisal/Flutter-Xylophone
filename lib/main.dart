a3056bd154eea0524386140cbcbbf75a787fbc4c
Faisal Alkheraiji <FaisalAlkheraiji@gmail.com>
2020-04-20 20:21:08 +0300

    Update README and bulid UI with Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,10 +8,37 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildkey(color: Colors.red, soundNumber: 1),
+              buildkey(color: Colors.orange, soundNumber: 2),
+              buildkey(color: Colors.yellow, soundNumber: 3),
+              buildkey(color: Colors.green, soundNumber: 4),
+              buildkey(color: Colors.teal, soundNumber: 5),
+              buildkey(color: Colors.blue, soundNumber: 6),
+              buildkey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildkey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(soundNumber);
+          }),
     );
   }
 }
